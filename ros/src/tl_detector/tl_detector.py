9fe705af85002d1607c1a315376543c537fc257a
gaurav2205 <gaurav.garg@gmx.de>
2019-06-02 22:11:37 +0200

    Add files via upload

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -7,13 +7,10 @@ from styx_msgs.msg import Lane
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from light_classification.tl_classifier import TLClassifier
-from scipy.spatial import KDTree
 import tf
 import cv2
 import yaml
-import numpy as np
-import math
-import csv
+from scipy.spatial import KDTree
 
 STATE_COUNT_THRESHOLD = 3
 
@@ -25,9 +22,6 @@ class TLDetector(object):
         self.waypoints = None
         self.camera_image = None
         self.lights = []
-
-	self.waypoints_2D = None
-	self.waypoint_tree = None
 
         sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
@@ -55,7 +49,6 @@ class TLDetector(object):
         self.last_state = TrafficLight.UNKNOWN
         self.last_wp = -1
         self.state_count = 0
-	self.img_number = 0
 
         rospy.spin()
 
@@ -64,9 +57,6 @@ class TLDetector(object):
 
     def waypoints_cb(self, waypoints):
         self.waypoints = waypoints
-	if not self.waypoints_2D:
-		self.waypoints_2D = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in self.waypoints.waypoints]
-		self.waypoint_tree = KDTree(self.waypoints_2D)
 
     def traffic_cb(self, msg):
         self.lights = msg.lights
@@ -101,7 +91,7 @@ class TLDetector(object):
             self.upcoming_red_light_pub.publish(Int32(self.last_wp))
         self.state_count += 1
 
-    def get_closest_waypoint(self, pose):
+    def get_closest_waypoint(self, x,y):
         """Identifies the closest path waypoint to the given position
             https://en.wikipedia.org/wiki/Closest_pair_of_points_problem
         Args:
@@ -112,23 +102,9 @@ class TLDetector(object):
 
         """
         #TODO implement
-	x = pose.position.x
-	y = pose.position.y
-	closest_idx = self.waypoint_tree.query([x, y], 1)[1]
-
-	closest_coord = self.waypoints_2D[closest_idx]
-	prev_coord = self.waypoints_2D[closest_idx-1]
-
-	cl_vect = np.array(closest_coord)
-	prev_vect = np.array(prev_coord)
-	pos_vect = np.array([x, y])
-
-	val = np.dot(cl_vect - prev_vect, pos_vect - cl_vect)
-
-	if val > 0:
-		closest_idx = (closest_idx + 1) % len(self.waypoints_2D)
-
-	return closest_idx
+		
+		closest_idx=self.waypoint_tree.query([x,y],1)[1]
+        return closest_idx
 
     def get_light_state(self, light):
         """Determines the current color of the traffic light
@@ -140,25 +116,15 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        if(not self.has_image):
-            self.prev_light_loc = None
-            return False
+	return light.state
+        # if(not self.has_image):
+            # self.prev_light_loc = None
+            # return False
 
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        # cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
-	###gathering data from manual driving
-	data_dir = "training_data/img" + str(self.img_number) + ".jpeg"
-	self.img_number += 1
-	cv2.imwrite(data_dir, cv_image)
-	row = [data_dir, str(self.lights[light-1].state)]
-	
-	with open('training_data/data.csv', 'a') as csvfile:
-		filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-		filewriter.writerow(row)
-	###
-
-        #Get classification
-        return self.light_classifier.get_classification(cv_image)
+        # #Get classification
+        # return self.light_classifier.get_classification(cv_image)
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
@@ -170,48 +136,35 @@ class TLDetector(object):
 
         """
         light = None
+        line_wp_idx = None
+		
 
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
-	stop_lines_tree = KDTree(stop_line_positions)
-
         if(self.pose):
-            car_position = self.get_closest_waypoint(self.pose.pose)
-	    pose_new = self.waypoints_2D[car_position]
+		car_position = self.get_closest_waypoint(self.pose.pose.position.x,self.pose.pose.position.y)
+		
 
         #TODO find the closest visible traffic light (if one exists)
+	diff = len(self.waypoints.waypoints)
+	for i, light in enumerate(self.lights):
+		#Get stop line waypoint index
+		line = stop_line_positions[i]
+		temp_wp_idx = self.get_closest_waypoint(line[0],line[1])
+		#Find the closest stop line waypoint index
+		d=temp_wp_idx-car_position
+		if d >= 0 and d < diff:
+			diff = d
+			closest_light =  light
+			line_wp_idx=temp_wp_idx
+				 
+				
 
-	###checking if next traffic light is within 100m 
-	x = pose_new[0]
-	y = pose_new[1]
-	closest_idx = stop_lines_tree.query([x, y], 1)[1]
-
-	closest_coord = stop_line_positions[closest_idx]
-	prev_coord = stop_line_positions[closest_idx-1]
-
-	cl_vect = np.array(closest_coord)
-	prev_vect = np.array(prev_coord)
-	pos_vect = np.array([x, y])
-
-	val = np.dot(cl_vect - prev_vect, pos_vect - cl_vect)
-
-	if val > 0:
-		closest_idx = (closest_idx + 1) % len(self.waypoints_2D)
-
-	light_pos = stop_line_positions[closest_idx]
-
-	dist = math.sqrt((light_pos[0] - x)*(light_pos[0] - x) + (light_pos[1] - y)*(light_pos[1] - y))
-	if dist < 100:
-		light = closest_idx + 1
-		light_wp = self.waypoint_tree.query([light_pos[0], light_pos[1]], 1)[1]
-	###
-
-        if light:
-            state = self.get_light_state(light)
-            return light_wp, state
-	    
-        self.waypoints = None
-        return -1, TrafficLight.UNKNOWN
+	if closest_light:
+		state = self.get_light_state(closest_light)
+		return line_wp_idx, state
+	self.waypoints = None
+	return -1, TrafficLight.UNKNOWN
 
 if __name__ == '__main__':
     try:
