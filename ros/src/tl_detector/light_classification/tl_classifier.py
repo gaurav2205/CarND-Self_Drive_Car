d6b72577ada3965af4cf386e70d8748f3ff37fbb
gaurav2205 <gaurav.garg@gmx.de>
2019-06-11 14:38:57 +0200

    New classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,37 +1,67 @@
-#Nobuyuki Tomatsu
-#2019/06/02 Implement traffic light state classification.
-
-#from styx_msgs.msg import TrafficLight
-import numpy as np
+from styx_msgs.msg import TrafficLight
 import tensorflow as tf
+import numpy as np
 import cv2
+import rospy
+import datetime
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        self.resized_width=32
-        self.resized_height=32
-        self.sess=tf.Session()
-        self.saver=tf.train.import_meta_graph("./classifier_model/tl_classifier.ckpt.meta")
-        self.saver.restore(self.sess, "./classifier_model/tl_classifier.ckpt")
-        self.x_input=self.sess.graph.get_tensor_by_name("x_input:0")
-        self.prediction=self.sess.graph.get_tensor_by_name("prediction:0")
-        self.data=np.zeros([1,self.resized_height,self.resized_width,3])
+
+        PATH_TO_GRAPH = 'light_classification/model/frozen_inference_graph_sim.pb'
+        self.graph = tf.Graph()
+        self.threshold = .5
+
+        with self.graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(PATH_TO_GRAPH, 'rb') as fid:
+                od_graph_def.ParseFromString(fid.read())
+                tf.import_graph_def(od_graph_def, name='')
+
+            self.image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
+            self.boxes = self.graph.get_tensor_by_name('detection_boxes:0')
+            self.scores = self.graph.get_tensor_by_name('detection_scores:0')
+            self.classes = self.graph.get_tensor_by_name('detection_classes:0')
+            self.num_detections = self.graph.get_tensor_by_name(
+                'num_detections:0')
+
+        self.sess = tf.Session(graph=self.graph)
+
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
-
         Args:
             image (cv::Mat): image containing the traffic light
-
         Returns:
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
         """
-        #TODO implement light color prediction
-        image_hsv=cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        self.data[0,:,:,:]=cv2.resize(image_hsv,(self.resized_height,self.resized_width))
-        #tl_state_pred=self.prediction.eval(feed_dict={self.x_input: self.data})
-        tl_state_pred=self.sess.run("prediction:0",feed_dict={"x_input:0":self.data})[0]
-        #RED=0 YELLOW=1 GREEN=2
-        return tl_state_pred
+        with self.graph.as_default():
+            img_expand = np.expand_dims(image, axis=0)
+            start = datetime.datetime.now()
+            (boxes, scores, classes, num_detections) = self.sess.run(
+                [self.boxes, self.scores, self.classes, self.num_detections],
+                feed_dict={self.image_tensor: img_expand})
+            end = datetime.datetime.now()
+            c = end - start
+            print(c.total_seconds())
+
+        boxes = np.squeeze(boxes)
+        scores = np.squeeze(scores)
+        classes = np.squeeze(classes).astype(np.int32)
+
+        print('SCORES: ', scores[0])
+        print('CLASSES: ', classes[0])
+
+        if scores[0] > self.threshold:
+            if classes[0] == 1:
+                print('GREEN')
+                return TrafficLight.GREEN
+            elif classes[0] == 2:
+                print('RED')
+                return TrafficLight.RED
+            elif classes[0] == 3:
+                print('YELLOW')
+                return TrafficLight.YELLOW
+
+        return TrafficLight.UNKNOWN
